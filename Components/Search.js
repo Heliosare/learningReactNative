19f70797560127015c2f1d16e54613c194116362
Unknown <vincent.galoger@gmail.com>
2019-11-02 19:37:39 +0100

    Adding image and loading
    https://openclassrooms.com/fr/courses/4902061-developpez-une-application-mobile-react-native/5359721-apprehendez-le-setstate

--- Components/Search.js
+++ Components/Search.js
@@ -1,7 +1,7 @@
 // Components/Search.js
 
 import React from 'react'
-import { StyleSheet, View, TextInput, Button, Text, FlatList } from 'react-native';
+import { StyleSheet, View, TextInput, Button, Text, FlatList, ActivityIndicator } from 'react-native';
 import { getFilmsFromApiWithSearchedText } from '../API/TMDBApi' ;
 import films from '../Helpers/filmsData';
 import FilmItem from './FilmItem';
@@ -14,14 +14,19 @@ class Search extends React.Component {
     this.setState({ films: data.results })
     this.state = {
       films: [],
+      isLoading: false,
     };
   }
 
   _loadFilms() {
     console.log(this.searchedText) // Un log pour vérifier qu'on a bien le texte du TextInput
     if (this.searchedText.length > 0) { // Seulement si le texte recherché n'est pas vide
+      this.setState({ isLoading: true }) // Lancement du chargement
       getFilmsFromApiWithSearchedText(this.searchedText).then(data => {
-          this.setState({ films: data.results })
+          this.setState({
+            films: data.results,
+            isLoading: false,
+          })
       })
     }
   }
@@ -30,18 +35,35 @@ class Search extends React.Component {
     this.searchedText = text;
   }
 
+  _displayLoading() {
+    if (this.state.isLoading) {
+      return (
+        <View style={styles.loading_container}>
+          <ActivityIndicator size='large' />
+          {/* Le component ActivityIndicator possède une propriété size pour définir la taille du visuel de chargement : small ou large. Par défaut size vaut small, on met donc large pour que le chargement soit bien visible */}
+        </View>
+      )
+    }
+  }
+
+
   render() {
+    console.log(this.state.isLoading)
     return (
       <View style={styles.main_container}>
-        <TextInput style={styles.textinput} placeholder='Titre du film'
+        <TextInput
+          style={styles.textinput}
+          placeholder='Titre du film'
           onChangeText={(text) => this._searchTextInputChanged(text)}
-                      />
+          onSubmitEditing={() => this._loadFilms()}
+        />
         <Button title='Rechercher' onPress={() => this._loadFilms()}/>
         <FlatList
           data={this.state.films}
           keyExtractor={(item) => item.id.toString()}
           renderItem={({item}) => <FilmItem film={item}/>}
         />
+        { this._displayLoading() }
       </View>
     )
   }
@@ -59,6 +81,15 @@ const styles = StyleSheet.create({
     borderColor: '#000000',
     borderWidth: 1,
     paddingLeft: 5
+  },
+  loading_container: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 100,
+    bottom: 0,
+    alignItems: 'center',
+    justifyContent: 'center'
   }
 })
 
